ae072ee7894f8a656b3b56323a293c296432a662
Sana Ayromlou <s.ayromlou@purang30.ece.ubc.ca>
2022-12-05 17:35:29 -0800

    virtual_dataset

--- federated/fed_Medical.py
+++ federated/fed_Medical.py
@@ -27,14 +27,15 @@ def get_cur_features(self, inputs, outputs):
 
 # def contrastive_loss(anchor_data_loader,model,inputs,targets,device):
 def contrastive_loss(s_centroids,t_centroids_norm,outputs,model,device):
-    ro = 0.9
-    temprature = 5
+    ro = 0.99
+    temprature = 0.1
     s_features = torch.zeros((model.fc.out_features, model.fc.in_features)).to(device)
+    # print(outputs)
     for i in range(len(s_features)):
         indices = torch.where(outputs==i)[0]
         if len(indices) != 0:
             s_features[i] = torch.sum(cur_features[indices],dim=0)/len(indices)
-    # print(s_features)
+    # print(s_features[0])
 
     s_centroids = (1-ro)*s_centroids.detach() + ro*s_features
     # print(s_centroids)
@@ -42,11 +43,10 @@ def contrastive_loss(s_centroids,t_centroids_norm,outputs,model,device):
     s_centroids_norm = s_centroids / (s_centroids.norm(dim=1)[:, None]+1e-10)
     res = torch.exp(torch.mm(t_centroids_norm, s_centroids_norm.transpose(0,1))/temprature)
 
-    loss4 = torch.sum(res)
-
-    # print(res,mask,torch.mul(res,mask))
-
-    loss4 = -1* torch.log(torch.sum(torch.diagonal(res,0)))+torch.log(torch.sum(res))
+    loss4 = -1* torch.sum(torch.log(torch.diagonal(res,0)))+torch.sum(torch.log(torch.sum(res,dim = 0)))
+    
+    # Loss = nn.MSELoss()
+    # loss4 = 100*Loss(t_centroids_norm,s_centroids_norm)
     
     return loss4,s_centroids
 
@@ -104,6 +104,8 @@ def train_virtual(model, train_loader, anchor_centroids ,optimizer, loss_fun, cl
     correct = 0
     loss_all = 0
     train_iter = iter(train_loader)
+    loss1s = 0
+    loss2s = 0
     s_centroids =  torch.zeros((model.fc.out_features, model.fc.in_features)).to(device)
     for step in range(len(train_iter)):
         optimizer.zero_grad()
@@ -114,16 +116,21 @@ def train_virtual(model, train_loader, anchor_centroids ,optimizer, loss_fun, cl
         output = model(x)
         # print(output.shape)
         # print(y.squeeze().shape)
-        loss1, s_centroids = contrastive_loss(s_centroids, anchor_centroids, output ,model,device)
+        pred = output.data.max(1)[1]
+        correct += pred.eq(y.view(-1)).sum().item()
+        
+        loss1, s_centroids = contrastive_loss(s_centroids, anchor_centroids, pred ,model,device)
         loss2 = loss_fun(output, y.squeeze())
+        # print(loss1,loss2)
         loss = loss1 + loss2
+        # loss = loss1
+        loss1s += loss1
+        loss2s += loss2
         loss.backward()
         loss_all += loss.item()
         optimizer.step()
-
-        pred = output.data.max(1)[1]
-        correct += pred.eq(y.view(-1)).sum().item()
-    return loss_all/len(train_iter), correct/num_data
+    # print(loss1s/len(train_iter),loss2s/len(train_iter))
+    return loss2s/len(train_iter),loss1s/len(train_iter) ,correct/num_data
 
 
 def train_fedprox(args, model, train_loader, optimizer, loss_fun, client_num, device):
@@ -176,8 +183,39 @@ def test(model, test_loader, loss_fun, device):
         pred = output.data.max(1)[1]
 
         correct += pred.eq(target.view(-1)).sum().item()
+        
     
     return test_loss/len(test_loader), correct /len(test_loader.dataset)
+
+
+def test_virtual(model, test_loader, anchor_centroids ,loss_fun, device):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    targets = []
+    loss1s = 0
+    loss2s = 0
+    s_centroids =  torch.zeros((model.fc.out_features, model.fc.in_features)).to(device)
+    for data, target in test_loader:
+        data = data.to(device).float()
+        target = target.to(device).long()
+        targets.append(target.detach().cpu().numpy())
+
+        output = model(data)
+        
+        pred = output.data.max(1)[1]
+
+        correct += pred.eq(target.view(-1)).sum().item()
+        
+    
+        loss1, s_centroids = contrastive_loss(s_centroids, anchor_centroids, pred ,model,device)
+        loss2 = loss_fun(output, target.squeeze()).item()
+        # print(loss1,loss2)
+        loss = loss1.item() + loss2
+        loss1s += loss1.item() 
+        loss2s += loss2
+        test_loss += loss
+    return loss2s/len(test_loader),loss1s/len(test_loader), correct /len(test_loader.dataset)
 
 
 def get_features_anchor(model, anchore_loader, device):
@@ -346,7 +384,8 @@ if __name__ == '__main__':
         print('Resume training from epoch {}'.format(resume_iter))
     else:
         resume_iter = 0
-
+    max_train_acc = 0
+    max_test_acc = 0
     # start training
     for a_iter in range(resume_iter, args.iters):
         optimizers = [optim.SGD(params=models[idx].parameters(), lr=args.lr) for idx in range(client_num)]
@@ -371,13 +410,17 @@ if __name__ == '__main__':
                     if a_iter > 0:
                         handle_cur_features = model.fc.register_forward_hook(get_cur_features)
                         train_virtual(model, train_loader, anchor_centroids,optimizer, loss_fun, client_num, device)
-                        handle_cur_features.remove()
                     else:
                         train(model, train_loader, optimizer, loss_fun, client_num, device)
                 else:
                     train(model, train_loader, optimizer, loss_fun, client_num, device)
-                train_loss, train_acc = test(model, train_loader, loss_fun, device)
-                test_loss, test_acc = test(model, test_loaders[client_idx], loss_fun, device)
+                if args.mode.lower() == 'virtual_data' and a_iter > 0:
+                    train_loss,train_loss_CN, train_acc = test_virtual(model, train_loader, anchor_centroids,loss_fun, device)
+                    test_loss,test_loss_CN, test_acc = test_virtual(model, test_loaders[client_idx],anchor_centroids, loss_fun, device)
+                    handle_cur_features.remove()
+                else:
+                    train_loss, train_acc = test(model, train_loader, loss_fun, device)
+                    test_loss, test_acc = test(model, test_loaders[client_idx], loss_fun, device)
                 if args.log:
                     metrics = {"Train_ACC_Local_"+str(client_idx): train_acc,
                               "Test_ACC_Local_"+str(client_idx): test_acc}
@@ -386,11 +429,21 @@ if __name__ == '__main__':
         # aggregation
         server_model, models = communication(args, server_model, models, client_weights)
         
+        if args.mode.lower() == 'virtual_data':
+            handle_cur_features = server_model.fc.register_forward_hook(get_cur_features)
+            anchor_centroids = get_features_anchor(server_model, anchor_loader,device)
+            handle_cur_features.remove()
+        
         # report after aggregation
         avg_train = 0
         for client_idx in range(client_num):
             model, train_loader, optimizer = models[client_idx], train_loaders[client_idx], optimizers[client_idx]
-            train_loss, train_acc = test(model, train_loader, loss_fun, device) 
+            if args.mode.lower() == 'virtual_data':
+                handle_cur_features = model.fc.register_forward_hook(get_cur_features)
+                train_loss, train_loss_CN,train_acc = test_virtual(model, train_loader, anchor_centroids,loss_fun, device)
+                handle_cur_features.remove()
+            else:
+                train_loss, train_acc = test(model, train_loader, loss_fun, device) 
             avg_train += train_acc
             print(' {:<11s}| Train Loss: {:.4f} | Train Acc: {:.4f}'.format(datasets[client_idx] ,train_loss, train_acc))
             if args.log:
@@ -398,6 +451,11 @@ if __name__ == '__main__':
                 metrics = {"Train_ACC_"+str(client_idx): train_acc,
                           "Train_Loss_"+str(client_idx): train_loss}
                 wandb.log(metrics)
+                if args.mode.lower() == 'virtual_data':
+                        metrics = {"Train_Loss_CN"+str(client_idx): train_loss_CN}
+                        wandb.log(metrics)
+        if max_train_acc < avg_train/client_num:
+                max_train_acc = avg_train/client_num
         if args.log:
             metrics = {"Train_AVG": avg_train/client_num}
             wandb.log(metrics)
@@ -405,7 +463,12 @@ if __name__ == '__main__':
         # start testing
         avg_test = 0
         for test_idx, test_loader in enumerate(test_loaders):
-            test_loss, test_acc = test(models[test_idx], test_loader, loss_fun, device)
+            if args.mode.lower() == 'virtual_data':
+                handle_cur_features = models[test_idx].fc.register_forward_hook(get_cur_features)
+                test_loss,test_loss_CN, test_acc = test_virtual(models[test_idx], test_loader, anchor_centroids,loss_fun, device)
+                handle_cur_features.remove()
+            else:
+                test_loss, test_acc = test(models[test_idx], test_loader, loss_fun, device)
             avg_test += test_acc
             print(' {:<11s}| Test  Loss: {:.4f} | Test  Acc: {:.4f}'.format(datasets[test_idx], test_loss, test_acc))
             if args.log:
@@ -413,9 +476,16 @@ if __name__ == '__main__':
                 metrics = {"Test_ACC_"+str(test_idx): test_acc,
                           "Test_Loss_"+str(test_idx): test_loss}
                 wandb.log(metrics)
+                if args.mode.lower() == 'virtual_data':
+                        metrics = {"Test_Loss_CN"+str(test_idx): test_loss_CN}
+                        wandb.log(metrics)
+        if max_test_acc < avg_test/client_num:
+                max_test_acc = avg_test/client_num
         if args.log:
             metrics = {"Test_AVG": avg_test/client_num}
             wandb.log(metrics)
+        print('Maximum train accuracy average is:',max_train_acc)
+        print('Maximum test accuracy average is:',max_test_acc)
 
     # Save checkpoint
     print(' Saving checkpoints to {}...'.format(SAVE_PATH))
