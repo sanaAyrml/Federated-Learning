b85bebb65987772712ecec8979edbc86793b0cdc
atrin78@student.ubc.ca <atrin78@student.ubc.ca>
2023-02-19 02:25:55 -0800

    ddd

--- federated/fed_digits_da_local.py
+++ federated/fed_digits_da_local.py
@@ -374,10 +374,10 @@ if __name__ == '__main__':
                                   domain_adv=domain_adv[client_idx], optimizer=optimizer, epoch=args.wk_iters, args=args,
                                   device=device,wandb=wandb,client_idx=client_idx)
                     elif args.synthesize_mode == 'global':
-                        train(args, wandb, model, train_loader, optimizer, loss_fun, client_num, device,
-                              client_idx, args.wk_iters)
+                        # train(args, wandb, model, train_loader, optimizer, loss_fun, client_num, device,
+                        #       client_idx, args.wk_iters)
 
-                        # train_multi_datasets(args, wandb,model, [train_loader, train_loader], optimizer, loss_fun, client_num, device,client_idx,args.wk_iters)
+                        train_multi_datasets(args, wandb,model, [train_loader, virtual_loaders[0]], optimizer, loss_fun, client_num, device,client_idx,args.wk_iters)
 
                         # train_uda(trg_loader=train_loader, src_loader=virtual_loaders[0], trg_model=model,
                         #           domain_adv=domain_adv[client_idx], optimizer=optimizer, epoch=args.wk_iters, args=args,
