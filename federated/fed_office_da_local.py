73781a070fa23bf58285cc475b493f74da5961a3
atrin78@student.ubc.ca <atrin78@student.ubc.ca>
2023-02-21 18:24:47 -0800

    office added

--- federated/fed_office_da_local.py
+++ federated/fed_office_da_local.py
@@ -16,6 +16,7 @@ import numpy as np
 import torchvision
 import torchvision.transforms as transforms
 from utils import data_utils
+from utils.data_utils import OfficeDataset
 import wandb
 import os
 from torch.utils.data import TensorDataset
