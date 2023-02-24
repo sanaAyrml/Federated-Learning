4870fd0b2664472e63486052ddccf33f9c24d224
atrin78@student.ubc.ca <atrin78@student.ubc.ca>
2023-02-24 14:28:48 -0800

    office added

--- federated/synthesize.py
+++ federated/synthesize.py
@@ -66,7 +66,7 @@ def src_img_synth_admm(gen_loader, src_model, args , device, mode, save_dir,a_it
             original_labels = torch.cat((original_labels, labels_real), 0)
 
     for i in range(args.iters_admm):
-        
+        torch.cuda.empty_cache()
         gc.collect()
 
         print(f'admm iter: {i}/{args.iters_admm}')
