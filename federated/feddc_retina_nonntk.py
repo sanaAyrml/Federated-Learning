7f3253d32263e5884b675a7f86bc6f6b5ec0eb68
atrin78@student.ubc.ca <atrin78@student.ubc.ca>
2023-03-01 16:10:56 -0800

    codes synced

--- federated/feddc_retina_nonntk.py
+++ federated/feddc_retina_nonntk.py
@@ -212,7 +212,7 @@ def prepare_data(args, datasets, public_dataset, im_size):
     # refuge_testset = RefugeDataset(data_base_path, transform=transform_refuge)
     # refuge_concated = torch.utils.data.ConcatDataset([refuge_trainset,refuge_valset,refuge_testset])
     print(torchvision.datasets.CIFAR10(root='./data', train=True,
-                                                  download=True, transform=transform_cifar)[:])
+                                                  download=True, transform=transform_cifar)[0])
     cifar_trainset = CustomDataset(torchvision.datasets.CIFAR10(root='./data', train=True,
                                                   download=True, transform=transform_cifar), transform=transform_cifar)
     cifar_virtualset = CustomDataset(torchvision.datasets.CIFAR10(root='./data', train=True,
