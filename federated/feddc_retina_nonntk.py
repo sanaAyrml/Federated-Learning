58fe3cb69a751460aca8f7532bf52130935148a8
atrin78@student.ubc.ca <atrin78@student.ubc.ca>
2023-03-01 17:20:33 -0800

    codes synced

--- federated/feddc_retina_nonntk.py
+++ federated/feddc_retina_nonntk.py
@@ -126,6 +126,11 @@ def prepare_data(args, datasets, public_dataset, im_size):
     #         transforms.RandomRotation((-30,30)),
     #         transforms.ToTensor(),
     # ])
+
+    transform_tensor = transforms.Compose([
+            transforms.ToTensor()
+    ])
+
     transform_unnormalized = transforms.Compose([
             transforms.Resize(im_size),            
             # transforms.RandomHorizontalFlip(),
@@ -214,11 +219,11 @@ def prepare_data(args, datasets, public_dataset, im_size):
     # print(torchvision.datasets.CIFAR10(root='./data', train=True,
     #                                               download=True, transform=transform_cifar)[0][0].shape)
     cifar_trainset = CustomDataset(torchvision.datasets.CIFAR10(root='./data', train=True,
-                                                  download=True), transform=transform_cifar)
+                                                  download=True, transform=transform_tensor), transform=transform_cifar)
     cifar_virtualset = CustomDataset(torchvision.datasets.CIFAR10(root='./data', train=True,
-                                                    download=True), transform=transform_cifar)
+                                                    download=True, transform=transform_tensor), transform=transform_cifar)
     cifar_testset = CustomDataset(torchvision.datasets.CIFAR10(root='./data', train=False,
-                                                 download=True), transform=transform_cifar)
+                                                 download=True, transform=transform_tensor), transform=transform_cifar)
 
     min_data_len = min(len(drishti_trainset), len(kaggle_trainset), len(rim_trainset), len(refuge_trainset),len(cifar_trainset))
 
